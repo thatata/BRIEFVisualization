b233cad3e891ba48ffbe7f6f7b1fa67c96b4cc56
Tarek Hatata <thatata@gwmail.gwu.edu>
2018-04-24 14:48:02 -0400

    added up-to-date modeling gui to the static folder

---
+++ cameraarm/app/modeling_static/ModelingWindow/build/CMakeFiles/ModelingWindow.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ModelingWindow.dir/src/ModelingMain.cxx.o"
+  "CMakeFiles/ModelingWindow.dir/src/modelingwindow.cxx.o"
+  "CMakeFiles/ModelingWindow.dir/src/modelingwindowstyle.cxx.o"
+  "ModelingWindow.pdb"
+  "ModelingWindow"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ModelingWindow.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- cameraarm/app/modeling_static/ModelingWindow/src/modelingwindowstyle.cxx
+++ cameraarm/app/modeling_static/ModelingWindow/src/modelingwindowstyle.cxx
@@ -40,6 +40,9 @@
 #include "modelingwindow.h"
 #include "modelingwindowstyle.h"
 
+//using namespace cv;
+typedef pcl::PointXYZ PointType;
+
 ModelingWindowStyle::ModelingWindowStyle() {
     // initialize struct for attributes
     attributes = new WindowStyleAttributes();
@@ -418,15 +421,16 @@ void ModelingWindowStyle::Snap() {
     }
 
     // use the x,y values of the drawn points to get the corresponding point in the point cloud
-//    pcl::PointXYZ bl = point_cloud.at(blDrawn[0], blDrawn[1]);
-//    pcl::PointXYZ tl = point_cloud.at(tlDrawn[0], tlDrawn[1]);
-//    pcl::PointXYZ tr = point_cloud.at(trDrawn[0], trDrawn[1]);
-//    pcl::PointXYZ br = point_cloud.at(brDrawn[0], brDrawn[1]);
+    pcl::PointXYZ bl = point_cloud.at(blDrawn[0], blDrawn[1]);
+    pcl::PointXYZ tl = point_cloud.at(tlDrawn[0], tlDrawn[1]);
+    pcl::PointXYZ tr = point_cloud.at(trDrawn[0], trDrawn[1]);
+    pcl::PointXYZ br = point_cloud.at(brDrawn[0], brDrawn[1]);
 
-    pcl::PointXYZ bl = point_cloud.at(106166);
-    pcl::PointXYZ tl = point_cloud.at(76971);
-    pcl::PointXYZ tr = point_cloud.at(98950);
-    pcl::PointXYZ br = point_cloud.at(127512);
+//    PREDETERMINED CORNERS BASED ON THE POINT CLOUD FOR THE STATIC EXAMPLE
+//    pcl::PointXYZ bl = point_cloud.at(106166);
+//    pcl::PointXYZ tl = point_cloud.at(76971);
+//    pcl::PointXYZ tr = point_cloud.at(98950);
+//    pcl::PointXYZ br = point_cloud.at(127512);
 
     // set z values for each drawn point
     blDrawn[0] = bl.x;
